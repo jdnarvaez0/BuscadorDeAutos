c91ed957014b2eb3044830de7fc06af74e031a2a
Juan Narvaez <juand.ns07@gmail.com>
2021-04-20 19:00:59 -0300

    filtrado y limpiarHtml

--- js/app.js
+++ js/app.js
@@ -7,23 +7,61 @@ const transmision = document.querySelector('#transmision');
 const color = document.querySelector('#color');
 const resultado = document.querySelector('#resultado');
 
-// crear los anios
-const years = document.createElement('option');
 const max = new Date().getFullYear();
 const min = max - 10;
 
-for( let i= max; i > min; i--){
-    const option = document.createElement('option');
-    option.value = i;
-    option.innerText = i;
-    document.querySelector('#year').appendChild(option);
+// Datos para la busqueda
+const datosBusqueda = {
+    marca : '',
+    year: '',
+    minimo : '',
+    maximo: '',
+    puertas: '',
+    transmision:'',
+    color:''
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     mostrarAutos(autos);
+
+    generarAnio();
 });
 
-function mostrarAutos() {
+marca.addEventListener('change', e => {
+    datosBusqueda.marca =  e.target.value;
+
+    filtraAutos();
+});
+
+year.addEventListener('change', e => {
+    datosBusqueda.year =  parseInt(e.target.value); 
+
+    filtraAutos();
+});
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo =  e.target.value
+});
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo =  e.target.value
+});
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas =  e.target.value
+});
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision =  e.target.value
+});
+
+color.addEventListener('change', e => {
+    datosBusqueda.color =  e.target.value
+});
+
+function mostrarAutos(autos) {
+    limpiarHTML();
+
     autos.forEach(auto => {
         const {marca, modelo, year, puertas, transmision, precio, color} = auto;
         const autoHTML = document.createElement('p');
@@ -38,4 +76,45 @@ function mostrarAutos() {
         // insertar en el HTML
         resultado.appendChild(autoHTML);
     });
+}
+
+// lipiarHTLM
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function generarAnio() {
+    for( let i= max; i >= min; i--){
+        const option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        year.appendChild(option);
+    }
+}
+
+function filtraAutos(){
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear);
+
+
+    // console.log(resultado);
+    mostrarAutos(resultado);
+}
+
+function filtrarMarca(auto){
+    const {marca} = datosBusqueda;
+    if ( marca ) {
+        return auto.marca === marca;
+    }
+    return auto;
+}
+
+function filtrarYear(auto){
+    const {year} = datosBusqueda;
+
+    if ( year ) {
+        return auto.year === year;
+    }
+    return auto
 }
